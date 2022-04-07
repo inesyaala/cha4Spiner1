a5e845661f5a273a198e456748d80390ccb97dc5
inesyaala <ines.benyaala@sesame.com.tn>
2022-04-07 01:39:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/cha4spiner/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.cha4spiner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner s,s1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        s=findViewById(R.id.Pays);
+        ArrayAdapter<CharSequence> adp1 = ArrayAdapter.createFromResource(this,R.array.liste_pays,android.R.layout.simple_list_item_1);
+        s.setAdapter(adp1);
+        String str[]={"a","b"};
+        s1 = findViewById(R.id.liste);
+        ArrayAdapter<String> adp2 =new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+    }
+}
